bdd91c0cc53c30ccafa12c25463b53e6e7c1b96c
Piotr Czyz <czpiotr@gmail.com>
2021-07-08 10:42:26 +0200

    Telegram Payments Bot - init project

---
+++ TelegramPay.Bot/src/CallbackEventArgs.cs
@@ -0,0 +1,10 @@
+namespace TelegramPay.Bot
+{
+    public class CallbackEventArgs
+    {
+        public string? Command { get; set; }
+        public int UserId { get; set; }
+        public long ChatId { get; set; }
+        public int MessageId { get; set; }
+    }
+}
