cdce907ec88c4f969c19497473ef341593a0116d
Rafał Cypcer <cypcer.rafal@gmail.com>
2021-07-08 15:09:34 +0200

    Microfrontend template with React and Vue
    [ADD] - SPA template

---
+++ search/ui/search-ui/src/react/root.component.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <div className="results-container">
+            <h2>Searched phrases:</h2>
+            <ul>
+                <li>Lorem ipsum</li>
+            </ul>
+        </div>
+    )
+}
+
+export default App
